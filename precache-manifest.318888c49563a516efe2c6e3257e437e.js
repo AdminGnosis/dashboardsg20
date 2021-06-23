fbd594fada3c380847e5b3e1b0fa0ad4249171b3
AdminGnosis <31543833+AdminGnosis@users.noreply.github.com>
2021-06-23 09:22:58 -0500

    Add files via upload

---
+++ precache-manifest.318888c49563a516efe2c6e3257e437e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e794274b55fa16e75933a16914b82cba",
+    "url": "/gestell/g20/index.html"
+  },
+  {
+    "revision": "88ad9de6bdc6af69b262",
+    "url": "/gestell/g20/static/css/3.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "360e4e1bb717d75cfeee",
+    "url": "/gestell/g20/static/css/main.78ba73a0.chunk.css"
+  },
+  {
+    "revision": "88ad9de6bdc6af69b262",
+    "url": "/gestell/g20/static/js/3.76753332.chunk.js"
+  },
+  {
+    "revision": "cec9a0aaf215407e35ee285de0acf1d6",
+    "url": "/gestell/g20/static/js/3.76753332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c613b757e52cefe0baa",
+    "url": "/gestell/g20/static/js/4.ad80ff3a.chunk.js"
+  },
+  {
+    "revision": "218803164cf87ead96a7",
+    "url": "/gestell/g20/static/js/5.5b519bfe.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/gestell/g20/static/js/5.5b519bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a3e795353d2899cf74",
+    "url": "/gestell/g20/static/js/6.b39b1f30.chunk.js"
+  },
+  {
+    "revision": "96c73944233a509a3aab13867e8f94e4",
+    "url": "/gestell/g20/static/js/6.b39b1f30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360e4e1bb717d75cfeee",
+    "url": "/gestell/g20/static/js/main.5598a31d.chunk.js"
+  },
+  {
+    "revision": "0f2aa19ac9c3b4fb0010",
+    "url": "/gestell/g20/static/js/runtime-main.5fd6460c.js"
+  },
+  {
+    "revision": "6f4519e26235c246af27",
+    "url": "/gestell/g20/static/js/xlsx.abce7707.chunk.js"
+  },
+  {
+    "revision": "99b2f8b11c91bc893f24de36010eca36",
+    "url": "/gestell/g20/static/media/logo.99b2f8b1.svg"
+  }
+]);
---
+++ static/js/xlsx.abce7707.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdashboard=this.webpackJsonpdashboard||[]).push([[2],{736:function(n,o){},822:function(n,o){},823:function(n,o){}}]);
+//# sourceMappingURL=xlsx.abce7707.chunk.js.map
